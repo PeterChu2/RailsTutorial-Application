b1ed94614f074753d7e078c7392fcd091f094957
Peter Chu <cgpchu@uwaterloo.ca>
2014-12-26 01:29:44 -0500

    Added functionality for following/unfollowing users, as well as ajax implementation of the follow button

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -67,5 +67,14 @@ class UsersControllerTest < ActionController::TestCase
     assert_not @user2.admin?
   end
 
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
 
 end
