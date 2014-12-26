e0afe8098fa8f07f5a7b274afb20a3fa004c1849
Peter Chu <cgpchu@uwaterloo.ca>
2014-12-26 02:25:28 -0500

    Add tests checking stats and checking feed microposts

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,5 +19,13 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do  |micropost|
       assert_match micropost.content, response.body
     end
+    # test the stats
+    assert_select 'div.stats'
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_select "a[href=?]", followers_user_path(@user)
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+    assert_select "a>strong.stat[id=?]", "following"
+    assert_select "a>strong.stat[id=?]", "followers"
   end
 end
